c67df98203bd485e064523041c2c85924d219eba
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-03-23 11:20:14 -0600

    setting up the connection

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "M@uiwowi30",
+    database: "trackerDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+    srcByRole();
+    initApplication();
+});
