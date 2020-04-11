6b47ff7321caefd02a3e922797c5f4b413c4edd6
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-04-11 11:57:20 -0600

    updating database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,8 @@
 DROP DATABASE IF EXISTS trackerDB;
-
--- create a database
 CREATE DATABASE trackerDB;
 
--- use the database you created 
 USE trackerDB;
 
--- create tables 
 CREATE TABLE employee
 (
     id INTEGER
@@ -27,57 +23,54 @@ CREATE TABLE employee
     (id)
 );
 
-    CREATE TABLE role
-    (
-        id INTEGER
-        AUTO_INCREMENT NOT NULL,
+CREATE TABLE role
+(
+	id INTEGER
+	AUTO_INCREMENT NOT NULL,
     title VARCHAR
-        (30),
+	(30),
     salary	DECIMAL
-        (10,2),
+	(10,2),
   
     department_id INT,
     PRIMARY KEY
-        (id)
+	(id)
 );
 
-        CREATE TABLE department
-        (
-            id INTEGER
-            AUTO_INCREMENT NOT NULL,
+CREATE TABLE department
+(
+	id INTEGER
+	AUTO_INCREMENT NOT NULL,
     name VARCHAR
-            (30),
+	(30),
     PRIMARY KEY
-            (id)
+	(id)
 );
 
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager
+	FROM employee e
+	LEFT JOIN employee m ON m.id = e.manager_id
+	INNER JOIN role r ON e.role_id = r.id
+	INNER JOIN department d ON r.department_id = d.id
+	ORDER BY e.id ASC;
 
-            -- RUN SEED FILE FIST --
+SELECT e.first_name, e.last_name, r.title, d.name
+	FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id
+	WHERE d.name = " ";
 
-            SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager
-            FROM employee e
-                LEFT JOIN employee m ON m.id = e.manager_id
-                INNER JOIN role r ON e.role_id = r.id
-                INNER JOIN department d ON r.department_id = d.id
-            ORDER BY e.id ASC;
+SELECT id, first_name, last_name, CONCAT_WS(" ", first_name, last_name) AS managers
+	FROM employee;
 
-            SELECT e.first_name, e.last_name, r.title, d.name
-            FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id
-            WHERE d.name = "Marketing";
+SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS employees
+	FROM employee;
 
-            SELECT id, first_name, last_name, CONCAT_WS(" ", first_name, last_name) AS managers
-            FROM employee;
+DELETE FROM employee WHERE id = 5;
 
-            SELECT id, first_name, last_name, CONCAT_WS(' ', first_name, last_name) AS employees
-            FROM employee;
+INSERT INTO  employee
+	(first_name, last_name, role_id, manager_id)
+VALUES
+	("Test", "Case", 5, 5);
 
-            DELETE FROM employee WHERE id = 5;
-
-            INSERT INTO  employee
-                (first_name, last_name, role_id, manager_id)
-            VALUES
-                ("Test", "Case", 5, 5);
-
-            SELECT e.first_name, e.last_name, r.title, d.name
-            FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id
-            WHERE e.manager_id = 1;
+SELECT e.first_name, e.last_name, r.title, d.name
+	FROM employee e INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id
+	WHERE e.manager_id = 1;
